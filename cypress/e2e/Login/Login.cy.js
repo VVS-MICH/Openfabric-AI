150b82d9903b1ace70ba0afe0d542fade6f9a003
Michael Adeoye <52085170+VVS-MICH@users.noreply.github.com>
2023-12-13 00:01:47 +0100

    Update Login.cy.js

--- cypress/e2e/Login/Login.cy.js
+++ cypress/e2e/Login/Login.cy.js
@@ -4,54 +4,41 @@
 describe('LOG IN PROCESSES', () => {
   
 
-  it.only('Verify that the user is able to log in with valid email and password', () => {
-    cy.visit('/')
-
+  it('Verify that the user is able to log in with valid email and password', () => {
+    cy.visit('/')    //visiting the testnet
+    cy.contains("Login with Email").should("exist").click()    //verifing we are in the log in page
+    cy.contains("Enter your email").should("exist")
+    cy.get("#mat-input-0").type("michaelifyadeoye@gmail.com")   // entering the emmail and password 
+    cy.get("#mat-input-1").type("password")
+         // clicking on log in 
+    cy.contains("Login").click()
+    cy.contains("Installed apps").should("exist").should("be.visible")     // asserting that the user is logged in to the app
   })
 
   it('Verify that the user is unable to log in with invalid email and password', () => {
-    // We'll store our item text in a variable so we can reuse it
-    const newItem = 'Feed the cat'
+    cy.visit('/')
+    cy.contains("Login with Email").should("exist").click()
+    cy.contains("Enter your email").should("exist")
+    cy.get("#mat-input-0").type("michaelifyadeoye@gmail.com")
+    cy.get("#mat-input-1").type("Wrongpassword")
+    cy.contains("Login").click()
+    cy.get("#mat-error-0").should("exist").should("be.visible")
+    cy.get("#mat-error-0").should("have.text", " Invalid credentials ")      // asserting that the user gets an error message
+    cy.contains("Installed apps").should("not.exist")       // asserting that the user is not logged in to the app
 
-    // Let's get the input element and use the `type` command to
-    // input our new list item. After typing the content of our item,
-    // we need to type the enter key as well in order to submit the input.
-    // This input has a data-test attribute so we'll use that to select the
-    // element in accordance with best practices:
-    // https://on.cypress.io/selecting-elements
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
 
-    // Now that we've typed our new item, let's check that it actually was added to the list.
-    // Since it's the newest item, it should exist as the last element in the list.
-    // In addition, with the two default items, we should have a total of 3 elements in the list.
-    // Since assertions yield the element that was asserted on,
-    // we can chain both of these assertions together into a single statement.
-    cy.get('.todo-list li')
-      .should('have.length', 3)
-      .last()
-      .should('have.text', newItem)
   })
 
   it('Verify that the user is able to log in with Metamask', () => {
-    // In addition to using the `get` command to get an element by selector,
-    // we can also use the `contains` command to get an element by its contents.
-    // However, this will yield the <label>, which is lowest-level element that contains the text.
-    // In order to check the item, we'll find the <input> element for this <label>
-    // by traversing up the dom to the parent element. From there, we can `find`
-    // the child checkbox <input> element and use the `check` command to check it.
-    cy.contains('Pay electric bill')
-      .parent()
-      .find('input[type=checkbox]')
-      .check()
-
-    // Now that we've checked the button, we can go ahead and make sure
-    // that the list element is now marked as completed.
-    // Again we'll use `contains` to find the <label> element and then use the `parents` command
-    // to traverse multiple levels up the dom until we find the corresponding <li> element.
-    // Once we get that element, we can assert that it has the completed class.
-    cy.contains('Pay electric bill')
-      .parents('li')
-      .should('have.class', 'completed')
+    cy.visit('/')
+    cy.contains("Login with Metamask").should("exist").click()
+    cy.contains("Enter your email").should("exist")
+    cy.get("#mat-input-0").type("michaelifyadeoye@gmail.com")
+    cy.get("#mat-input-1").type("Wrongpassword")
+    cy.get("#mat-error-0").should("exist").should("be.visible")
+    cy.contains("Login").click()
+    cy.get("#mat-error-0").should("have.text", " Invalid credentials ")
+    cy.contains("Installed apps").should("not.exist")
   })
 
   
